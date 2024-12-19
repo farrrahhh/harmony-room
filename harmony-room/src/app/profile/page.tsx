22901cd3f694fda361a0be6af7604953039a1ed4
Clement Nathanael Lim <18222032@mahasiswa.itb.ac.id>
2024-12-19 16:52:36 +0700

    mksh y farr :D

---
+++ harmony-room/src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+import UserProfile from '../../components/Profile';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <UserProfile/>
+        </div>
+    );
+};
+
+export default ProfilePage;
